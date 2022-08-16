6d4d2a2fa5a157127fc8ada1a466f1ddb18556d0
Fatuousnerd <calvinkmwaura@gmail.com>
2022-08-16 20:38:25 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const wrapper = document.querySelector('.wrapper'),
+generatebtn = wrapper.querySelector('.form button'),
+qrInput = wrapper.querySelector('.form input'),
+qrImg = wrapper.querySelector('.qr-code img')
+
+generatebtn.addEventListener('click', () => {
+    let qrValue = qrInput.value;
+    if(!qrValue) return
+    qrImg.src = ` https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+    console.log(qrValue);
+    wrapper.classList.add("active");
+});
