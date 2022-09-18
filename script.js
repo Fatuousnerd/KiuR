7917b3c63cbabdcf9db827971b3e1b827ece4a72
Fatuousnerd <calvinkmwaura@gmail.com>
2022-09-18 20:40:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ generatebtn.addEventListener('click', () => {
     console.log(qrValue);
     wrapper.classList.add("active");
 });
+
+wrapper.addEventListener("keypress", (e) => {
+    if(e.key == "Enter"){
+        let qrValue = qrInput.value;
+        if(!qrValue) return
+        qrImg.src = ` https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+        console.log(qrValue);
+        wrapper.classList.add("active");
+    }
+})
